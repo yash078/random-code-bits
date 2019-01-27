0ad7da132b447112cf6961694c063aeac62bb431
yash078 <43049879+yash078@users.noreply.github.com>
2019-01-27 18:09:45 -0500

    Update and rename bubble.cs to bin_search.cs

--- bin_search.cs
+++ bin_search.cs
@@ -1,25 +1,24 @@
-        static int BinarySearch(int[] haystack, int needle, ref int numOfComparison)
+        static int BinarySearch(int[] intArray, int needle)
         {
             int needleIndex = -1;
-            numOfComparison++;
 
-            int middlePoint = haystack.Length / 2;
-            if (haystack.Length >= 1) // breaking recursion
+            int middlePoint = intArray.Length / 2;
+            if (intArray.Length >= 1) // breaking recursion
             {
-                if (needle == haystack[middlePoint])
+                if (needle == intArray[middlePoint])
                 {
                     return middlePoint;
                 }
-                if (needle < haystack[middlePoint])
+                if (needle < intArray[middlePoint])
                 {
                     // if the term is found in the first half of the array
-                    return BinarySearch(haystack.Skip(0).Take(middlePoint).ToArray(), needle, ref numOfComparison);
+                    return BinarySearch(intArray.Skip(0).Take(middlePoint).ToArray(), needle);
                 }
-                else if (needle > haystack[middlePoint])
+                else if (needle > intArray[middlePoint])
                 {
                     // if the term is found in the second half of the array. !!! we need to keep track of the final recursion returns -1
                     // in such case, we break and return -1
-                    int recNeedleIndex = BinarySearch(haystack.Skip(middlePoint + 1).Take(haystack.Length - 1).ToArray(), needle, ref numOfComparison);
+                    int recNeedleIndex = BinarySearch(intArray.Skip(middlePoint + 1).Take(intArray.Length - 1).ToArray(), needle);
                     if (recNeedleIndex == -1)
                     {
                         return -1;
