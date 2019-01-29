8960c5862fdf7ff8dc96081d84abc0c6a622b5f8
yash078 <43049879+yash078@users.noreply.github.com>
2019-01-29 09:37:19 -0500

    code cleanup

--- self.py
+++ self.py
@@ -1,22 +1,9 @@
 def isSelfDescriptive(number):
-    numberAsStr = str(number)
-    numberAsStr = list(str(number))
-    numberAsList = [int(x) for x in numberAsStr]
-    numberLength = len(numberAsStr)
-    if int(max(numberAsStr)) > numberLength or sum(numberAsList) != numberLength:
+    numberAsList = [int(x) for x in list(str(number))]
+    numberLength = len(numberAsList)
+    if max(numberAsList) > numberLength or sum(numberAsList) != numberLength:
         return False
-#     Sum = [0] * (numberLength)
-#     for i in range(numberLength):
-#         Sum[i] = numberAsStr.count(str(i))
-#     # print(number, sum)
-    # return int(''.join([str(x) for x in Sum])) == number
-    return all(numberAsStr.count(str(i)) == int(ch) for i, ch in enumerate(numberAsStr))
-
-
-# def isSelfDescriptive(n):
-#     s = str(n)
-#     return all(s.count(str(i)) == int(ch) for i, ch in enumerate(s))
-
+    return all(numberAsList.count(i) == ch for i, ch in enumerate(numberAsList))
 
 for i in range(1000, 3300000):
     if isSelfDescriptive(i):
